ff48be650c036e89a580c905655187b596c1fc39
skye-lopez <sky.lopez.dev@gmail.com>
2024-09-06 15:03:40 -0700

    INIT project structre for cobra

---
+++ cmd/root.go
@@ -0,0 +1,24 @@
+package cmd
+
+import (
+	"os"
+
+	"github.com/spf13/cobra"
+)
+
+var rootCmd = &cobra.Command{
+	Use:   "go-get-cli",
+	Short: "CLI Tools for go get to manage and find pacakges",
+	Long:  "CLI Tools for go get to manage and find pacakges",
+}
+
+func init() {
+	// setup here (this gets run regardless of command)
+}
+
+func Execute() {
+	err := rootCmd.Execute()
+	if err != nil {
+		os.Exit(1)
+	}
+}
---
+++ main.go
@@ -0,0 +1,13 @@
+/*
+Copyright © 2024 Aria Lopez <aria.lopez.dev@gmail.com>
+*/
+
+package main
+
+import (
+	"github.com/skye-lopez/go-get-cli/cmd"
+)
+
+func main() {
+	cmd.Execute()
+}
